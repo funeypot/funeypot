8a3d3507c81877eee3c83d1cc21f938218787e3c
Jason Song <i@wolfogre.com>
2024-01-30 23:38:16 +0800

    fix: ssh version

--- internal/pkg/fakever/update.sh
+++ internal/pkg/fakever/update.sh
@@ -5,4 +5,5 @@ set -e
 curl -sSL 'https://raw.githubusercontent.com/openssh/openssh-portable/master/version.h' \
  | grep -E "^#define SSH_VERSION" \
  | awk -F "\"" '{print $2}' \
+ | tr -d '\n' \
  > ssh.txt
