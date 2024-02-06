6658961b7c0211fe530be1b6d583bfc2846de79e
Jason Song <i@wolfogre.com>
2024-02-06 18:24:50 +0800

    test: fix port

--- test/integration_test.go
+++ test/integration_test.go
@@ -22,7 +22,6 @@ func PrepareServers(t *testing.T, modifyConfig func(cfg *config.Config)) func() 
 		t.Fatalf("load config: %v", err)
 	}
 
-	cfg.Ssh.Address = ":2222"
 	cfg.Log.Level = "error"
 	cfg.Database.Dsn = filepath.Join(t.TempDir(), "funeypot.db")
 
--- test/ssh_test.go
+++ test/ssh_test.go
@@ -7,21 +7,25 @@ import (
 	"testing"
 	"time"
 
+	"github.com/wolfogre/funeypot/internal/app/config"
+
 	"github.com/stretchr/testify/assert"
 	"golang.org/x/crypto/ssh"
 )
 
 func TestSshServer(t *testing.T) {
-	defer PrepareServers(t, nil)()
+	defer PrepareServers(t, func(cfg *config.Config) {
+		cfg.Ssh.Address = ":2222"
+	})()
 
-	config := &ssh.ClientConfig{
+	sshConfig := &ssh.ClientConfig{
 		User:            "username",
 		Auth:            []ssh.AuthMethod{ssh.Password("password")},
 		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
 	}
 
 	start := time.Now()
-	_, err := ssh.Dial("tcp", "127.0.0.1:22", config)
+	_, err := ssh.Dial("tcp", "127.0.0.1:2222", sshConfig)
 	assert.ErrorContains(t, err, "ssh: handshake failed: ssh: unable to authenticate")
 	assert.Greater(t, time.Since(start), 2*time.Second)
 }
