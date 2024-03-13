8d317bd30fcbaafcbba9a1f80b606aa4be0a6293
Jason Song <i@wolfogre.com>
2024-03-13 11:08:38 +0800

    test: more case

--- internal/pkg/abuseipdb/abuseipdb_test.go
+++ internal/pkg/abuseipdb/abuseipdb_test.go
@@ -16,8 +16,6 @@ import (
 func TestClient_Report(t *testing.T) {
 	httpmock.Activate()
 	defer httpmock.DeactivateAndReset()
-	client := NewClient("test_key", time.Second)
-
 	t.Run("Cooldown", func(t *testing.T) {
 		defer httpmock.Reset()
 		httpmock.RegisterResponder("POST", ReportUrl,
@@ -33,6 +31,8 @@ func TestClient_Report(t *testing.T) {
 				resp.Header.Set("Retry-After", "60")
 				return resp, nil
 			})
+		client := NewClient("test_key", time.Second)
+
 		until, ok := client.Cooldown()
 		assert.False(t, ok)
 		assert.Zero(t, until)
@@ -44,5 +44,35 @@ func TestClient_Report(t *testing.T) {
 		until, ok = client.Cooldown()
 		assert.True(t, ok)
 		assert.WithinDuration(t, time.Now().Add(60*time.Second), until, 2*time.Second)
+	})
+
+	t.Run("Cooldown invalid Retry-After", func(t *testing.T) {
+		defer httpmock.Reset()
+		httpmock.RegisterResponder("POST", ReportUrl,
+			func(request *http.Request) (*http.Response, error) {
+				resp := httpmock.NewStringResponse(429, `{
+  "errors": [
+      {
+          "detail": "Daily rate limit of 1000 requests exceeded for this endpoint. See headers for additional details.",
+          "status": 429
+      }
+  ]
+}`)
+				resp.Header.Set("Retry-After", "invalid")
+				return resp, nil
+			})
+		client := NewClient("test_key", time.Second)
+
+		until, ok := client.Cooldown()
+		assert.False(t, ok)
+		assert.Zero(t, until)
+
+		score, err := client.ReportSsh(context.Background(), "127.0.0.1", time.Now(), "test")
+		assert.ErrorContains(t, err, "429")
+		assert.Zero(t, score)
+
+		until, ok = client.Cooldown()
+		assert.False(t, ok)
+		assert.Zero(t, until)
 	})
 }
